2ee88746045ce625d7eca75eb4af3e83fdd6b994
Emmanuel Menyaga <sparklinsparky@gmail.com>
2023-05-22 17:32:01 +0100

    feat: implement login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,59 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import textStyles from "./text.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+      <div>
+        <div className={styles.leftContent}>
+          <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              src="/payeasy.svg"
+              alt="PayEasy Logo"
+              width={57}
+              height={66}
               priority
             />
-          </a>
+
+            <div className={styles.textContent}>
+              <h1 className={styles.title}>Monitor Your Transactions!</h1>
+
+              <p className={styles.body}>
+                Enter your merchant code to monitor your PayEasy transactions
+                made through this terminal
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+      <div>
+        <form action="" className={styles.form}>
+          <div>
+            <div className={styles.payeasyInput}>
+              <label htmlFor="" className={textStyles.bodyLarge}>
+                Code
+              </label>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <input type="text" placeholder="Enter merchant code" />
+            </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+            <div className={styles.payeasyInput}>
+              <label htmlFor="" className={textStyles.bodyLarge}>
+                Terminal ID (optional)
+              </label>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+              <input type="text" placeholder="Enter store terminal ID" />
+            </div>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <button
+            className={`${styles.payeasyButton} ${textStyles.bodyMedium}`}
+          >
+            Monitor Transactions
+          </button>
+        </form>
       </div>
     </main>
-  )
+  );
 }
